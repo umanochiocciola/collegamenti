52d3453e47ad09bd5f63bfc1d667970cdab6430e
umanochiocciola <59803009+umanochiocciola@users.noreply.github.com>
2021-02-11 09:34:51 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'encription69',
+  packages = ['encription69'],
+  version = '0.1',
+  license='DoWhateverYouWant',
+  description = 'a very weak encription system',
+  author = 'umanochiocciola',
+  author_email = 'HALLO',
+  url = 'https://github.com/umanochiocciola/encription69',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['encription', 'joke', '69', 'bruh'],
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: DoWhateverYouWhant',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
